725516702d78516e90dc67c1b76cf11045ed9143
AlfredoNQ <alfredoquinta@hotmail.com>
2026-08-26 18:05:13 -0300

    Publica alinhamento geral web iPhone v169

--- _worker.js
+++ _worker.js
@@ -667,7 +667,7 @@ export default {
       request.method === 'GET' &&
       (url.pathname === '/' || url.pathname === '/index.html')
     ) {
-      const assetUrl = new URL('/index.html?v=168', url.origin);
+      const assetUrl = new URL('/index.html?v=169', url.origin);
       const assetReq = new Request(assetUrl.toString(), {
         method:'GET',
         headers:request.headers
@@ -675,7 +675,7 @@ export default {
       const asset = await env.ASSETS.fetch(assetReq);
       const headers = new Headers(asset.headers);
       headers.set('Cache-Control','no-store, no-cache, must-revalidate, max-age=0');
-      headers.set('X-Gado-App-Version','168');
+      headers.set('X-Gado-App-Version','169);
       return new Response(asset.body,{
         status:asset.status,
         statusText:asset.statusText,
